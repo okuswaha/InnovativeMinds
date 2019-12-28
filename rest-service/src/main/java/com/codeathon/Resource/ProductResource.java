63ac318ddbc93843b134437d381253e2209827ad
OmPrakash Kuswaha <okuswaha@routeone.com>
2019-12-28 10:14:15 -0500

    adding more crud opereations on DynamoDB

--- rest-service/src/main/java/com/codeathon/Resource/ProductResource.java
+++ rest-service/src/main/java/com/codeathon/Resource/ProductResource.java
@@ -3,6 +3,7 @@ package com.codeathon.Resource;
 import com.codeathon.repositories.ProductRepository;
 import com.codeathon.tables.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +12,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductResource {
     @Autowired
     private ProductRepository productRepository;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String welcome(){
-        return "Welcome";
+    @RequestMapping(method = RequestMethod.GET, path = "/initialize")
+    public Iterable<Product> initializeProduct(){
+        return productRepository.saveAll(buildProductsList());
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/get-products")
-    public List<Product> getProducts(){
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Product> getProducts(){
         productRepository.findAll().forEach(System.out::println);
-        return new ArrayList<>();
+        return productRepository.findAll();
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Product insert(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    private List<Product> buildProductsList() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Paper","Used for writing"));
+        products.add(new Product("Rock","Used for weight to hold papers on desk"));
+        products.add(new Product("Scissors","Used for cutting"));
+        return products;
 
     }
 }
