f061bd9359f994c8e6062f0b1a490c472e0c7c24
OmPrakash Kuswaha <okuswaha@routeone.com>
2019-12-28 01:45:26 -0500

    fixing spring boot dependencies , configuring to connect to AWS DynamoDB Table, adding findAll call to Products DynamoDB Table

--- rest-service/src/main/java/com/codeathon/Resource/ProductResource.java
+++ rest-service/src/main/java/com/codeathon/Resource/ProductResource.java
@@ -1,13 +1,29 @@
 package com.codeathon.Resource;
 
+import com.codeathon.repositories.ProductRepository;
+import com.codeathon.tables.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductResource {
+    @Autowired
+    private ProductRepository productRepository;
+
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String welcome(){
         return "Welcome";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/get-products")
+    public List<Product> getProducts(){
+        productRepository.findAll().forEach(System.out::println);
+        return new ArrayList<>();
+
     }
 }
---
+++ rest-service/src/main/java/com/codeathon/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.codeathon.repositories;
+
+import com.codeathon.tables.Product;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface ProductRepository extends CrudRepository<Product, String> {
+}
